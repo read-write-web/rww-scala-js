4b1d5ad5a92cc42e37c55c6192cb1fab839f41b4
Henry Story <henry.story@bblfish.net>
2015-02-20 21:58:14 +0100

    simplification and clean up of WebClient

--- src/main/scala/rww/ui/WebClient.scala
+++ src/main/scala/rww/ui/WebClient.scala
@@ -1,55 +1,42 @@
 package rww.ui
 
-import java.io.{ByteArrayOutputStream, StringReader}
+import java.io.StringReader
 
-import org.scalajs.dom._
+import org.scalajs.dom
 import org.scalajs.dom.ext._
 import org.w3.banana._
 import org.w3.banana.plantain.Plantain
 import org.w3.banana.plantain.Plantain.ops._
 
 import scala.concurrent._
-import scala.util.{Failure, Success}
 
 object WebClient {
+  import rww.rdf._
 
   /**
    * Issue an AJAX GET request to get a remote foaf profile
    */
   def getProfile(foafProfileUri: Plantain#URI)(implicit ec: ExecutionContext): Future[PointedGraph[Plantain]] = {
-    val responseF = Ajax.get(foafProfileUri.fragmentLess.toString, headers = Map("Accept" -> "application/n-triples"))
-    readResponse(foafProfileUri, foafProfileUri.fragmentLess)(responseF)
+    val base = foafProfileUri.fragmentLess.toString
+    for {
+      xhr <- Ajax.get(foafProfileUri.fragmentLess.toString, headers = Map("Accept" -> "application/n-triples"))
+        g <- ntriplesReader.read(new StringReader(xhr.responseText), base.toString).asFuture
+      // one should then later also add the graph to a store
+      //      _ <- appendToGraph(rww.rdf.jsstore, bblDocUri, g) //add to store
+      //      graph <- getGraph(rww.rdf.jsstore,bblDocUri ) //get from store
+      } yield PointedGraph[Plantain](foafProfileUri, g)
   }
 
   /**
    * Issue an AJAX PUT request to update a remote foaf profile
    */
-  def putProfile(newGraph: PointedGraph[Plantain])(implicit ec: ExecutionContext): Future[PointedGraph[Plantain]] = {
-    var os = new ByteArrayOutputStream()
-    val writeTry = Plantain.ntriplesWriter.write(newGraph.graph, os, "")
-    writeTry match {
-      case Success(u) => {
+  def putProfile(newGraph: PointedGraph[Plantain])(implicit ec: ExecutionContext): Future[dom.XMLHttpRequest] = {
+    val writeTry = ntriplesWriter.asString(newGraph.graph, "")
+    writeTry.asFuture.flatMap { nt =>
         val profileUri = URI(newGraph.pointer.toString)
-        val res = Ajax.put(profileUri.fragmentLess.toString, os.toString, headers = Map("Accept" -> "application/n-triples"))
-        readResponse(profileUri, profileUri.fragmentLess)(res)
-      }
-      case Failure(t) => Future.failed(t)
+        Ajax.put(profileUri.fragmentLess.toString, nt, headers = Map("Accept" -> "application/n-triples"))
     }
   }
 
-  def readResponse(pointer: Plantain#URI, base: Plantain#URI)(response: Future[XMLHttpRequest])(implicit ec: ExecutionContext): Future[PointedGraph[Plantain]] = {
-    response flatMap { xhr =>
-      val t = for {
-        g <- Plantain.ntriplesReader.read(new StringReader(xhr.responseText), base.toString)
-        // one should then later also add the graph to a store
-        //      _ <- appendToGraph(rww.rdf.jsstore, bblDocUri, g) //add to store
-        //      graph <- getGraph(rww.rdf.jsstore,bblDocUri ) //get from store
-      } yield PointedGraph[Plantain](pointer, g)
-      t match {
-        case Success(pg) => Future.successful(pg)
-        case Failure(t)  => Future.failed(t)
-      }
-    }
-  }
 
 }
